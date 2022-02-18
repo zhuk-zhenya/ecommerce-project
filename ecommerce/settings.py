d515742ec90c8095ec1a9388c47bc6fa0c7d0070
zhuk-zhenya <88724582+zhuk-zhenya@users.noreply.github.com>
2022-02-18 13:30:00 +0300

    add bootstrap and ststic files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR, 'ecommerce/static/',
 
+]
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
